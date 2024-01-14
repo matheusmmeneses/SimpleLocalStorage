85ff8e7c9380833f0204f480b34893f66b336e01
Matheus <91149331+matheusmmeneses@users.noreply.github.com>
2024-01-14 18:35:48 -0300

    fix

--- src/store.js
+++ src/store.js
@@ -5,24 +5,21 @@ export const useMainStore = defineStore({
     state: () => ({
         todos: [],
     }),
-    
     getters: {
         getAllTodos() {
             return this.todos
         },
-
         todoEmpty() {
             return this.todos.length <= 0
-        },
+        }
     },
-
     actions: {
         addTodo(todo) {
-            this.todos.push(todo)
+            this.todos.push(todo);
         },
-
         removeTodo(index) {
             this.todos.splice(index, 1)
-        },
+        }
+
     },
 })
